b8aabb587e872376315cbeeec9f65c4f09a850d6
Yippers <>
2018-03-16 11:04:49 -0700

    Add utility function wrapList()

--- src/utility.ts
+++ src/utility.ts
@@ -1,14 +1,12 @@
 // Copyright (C) 2018 Rimeto, LLC. All Rights Reserved.
 
-import { JSONValue } from './index';
-
-export function splitList(value: JSONValue, delimiter: string = ',') {
-  if (typeof value !== 'string') {
-    throw Error('cannot split non-string value: ' + value);
-  }
-
+export function splitList(value: string, delimiter: string = ';'): string[] {
   return value
     .split(delimiter)
     .map((s) => s.trim())
     .filter(Boolean);
+}
+
+export function wrapList(obj: any) {
+  return obj ? [obj] : [];
 }
