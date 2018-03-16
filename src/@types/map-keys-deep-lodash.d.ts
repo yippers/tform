2b2cb7a6a08d434affc8c3d7d669eb2c2f25ef28
Yippers <Yatharth@yatharth-mbp.local>
2018-03-16 11:14:17 -0700

    Reduce dependencies

--- src/@types/map-keys-deep-lodash.d.ts
+++
@@ -1,6 +0,0 @@
-// Copyright (C) 2018 Rimeto, LLC. All Rights Reserved.
-
-declare module 'map-keys-deep-lodash' {
-  function mapKeysDeepLodash(obj: any, cb: (value: any, key: any) => any): any;
-  export = mapKeysDeepLodash;
-}
